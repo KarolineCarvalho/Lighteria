66820c591db704245bc03b24dbf9f7b20f256903
Karoline Carvalho <ka.olicarvalho@gmail.com>
2021-09-09 11:22:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#f4f0fa'}}>
+      <View>
+        <Text> Get ready for the Gliding Champion of Mondstadt!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
